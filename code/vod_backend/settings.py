6fc222f572bc0fc5a1087e6803334dfe531ad138
altrauser <altrauser@mtsnow-load2.scc-lab.amperecomputing.com>
2023-10-24 16:42:31 -0700

    modified:   code/vod_backend/settings.py

--- code/vod_backend/settings.py
+++ code/vod_backend/settings.py
@@ -46,10 +46,12 @@ INSTALLED_APPS = [
     # 'uploadvideo',
     # 'processvideoapp',
     'videoplayer',
+    'corsheaders',
     # 'uploadpod',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -57,6 +59,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOWED_ORIGIN_REGEXES = [
+r"^https://\w+\.hhii\.ampere$",
 ]
 
 ROOT_URLCONF = 'vod_backend.urls'
